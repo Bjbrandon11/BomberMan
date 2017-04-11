b05a346c0f6f1084412c23f6bcae251b986fd311
Bjbrandon11 <brandon.rosa.553@k12.friscoisd.org>
2017-04-11 09:16:15 -0500

    Fix

--- BomberMan/BomberMan/Player.cs
+++ BomberMan/BomberMan/Player.cs
@@ -1,35 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-<<<<<<< HEAD
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-=======
 using System.Text;
->>>>>>> refs/remotes/origin/master
 
 namespace BomberMan
 {
     class Player
     {
-<<<<<<< HEAD
-        public PlayerIndex PlayerNum;//This is which gamePad the player is using
-        GamePadState oldGPState;//This is the state of the gamePad for the previous frame.
-        Vector2 position;
-
-        public Player(PlayerIndex index, Vector2 position)
-        {
-            PlayerNum = index;
-            GamePadState = GamePad.GetState(PlayerNum);
-            this.position = position;
-        }
-
-=======
->>>>>>> refs/remotes/origin/master
     }
 }
