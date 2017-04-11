bd2ec16a5f39176002aa114a4497ddc162d30544
Bjbrandon11 <brandon.rosa.553@k12.friscoisd.org>
2017-04-11 09:13:26 -0500

    Revert "Merge remote-tracking branch 'refs/remotes/origin/Entity'"
    This reverts commit a33d118bcad2ef3e25f6871b1d750df97342bf45, reversing
    changes made to de848ef05ef75afbbfa9ecb25d45b93b76f39c16.

--- BomberMan/BomberMan/Game1.cs
+++ BomberMan/BomberMan/Game1.cs
@@ -46,7 +46,7 @@ namespace BomberMan
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            GameHolder.game = this;
+
             // TODO: use this.Content to load your game content here
         }
 
