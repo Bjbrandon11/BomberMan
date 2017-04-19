c81c15027580721cde64c0fa48a693b973f61f3d
gcjunkin <george.junkin.470@k12.friscoisd.org>
2017-04-19 08:56:28 -0500

    Added a bomb class
    Ended the errors encountered by not having a bomb class

---
+++ BomberMan/BomberMan/Bomb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+
+namespace BomberMan
+{
+    class Bomb: Entity
+    {
+        int radius;
+        Vector2 location;
+        Timer animTimer;
+
+        public override void Update()
+        {
+            //TODO- write update logic
+            animTimer.Update();
+        }
+    }
+}
