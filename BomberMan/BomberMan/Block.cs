24daa1fb9456623987a5b4088b2ce7b98d68e940
gcjunkin <123470@ads.friscoisd.org>
2017-04-26 09:39:08 -0500

    changed protection level
    quick bug fix

--- BomberMan/BomberMan/Block.cs
+++ BomberMan/BomberMan/Block.cs
@@ -16,14 +16,14 @@ namespace BomberMan
             Explosion,
         }
         Entity item;
-        public const int SIZE= 108;
+        public const int SIZE = 108;
         //public readonly bool breakable;
         //public bool broken;
-        Tile text;
-        Rectangle hitBox;
-        BlockState currentState;
-        public Block(int x,int y):this(x,y, new Tile(0, 0, 32, 32, Tile.TextureList["Tiles/Block_Invin"]),BlockState.Passable){}
-        public Block(int x, int y,Tile text,BlockState state)
+        public Tile text;
+        public Rectangle hitBox;
+        public BlockState currentState;
+        public Block(int x, int y) : this(x, y, new Tile(0, 0, 32, 32, Tile.TextureList["Tiles/Block_Invin"]), BlockState.Passable) { }
+        public Block(int x, int y, Tile text, BlockState state)
         {
             this.text = text;
             hitBox = new Rectangle(x, y, SIZE, SIZE);
